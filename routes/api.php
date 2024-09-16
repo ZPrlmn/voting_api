9aa968e074609a98060b46b405da4a7aca1fe9da
ZPrlmn <134649026+ZPrlmn@users.noreply.github.com>
2024-09-16 14:05:32 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -7,16 +7,3 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\VoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::apiResource('user', UserController::class);
-Route::patch('/users/{id}/has_voted', [UserController::class, 'updateHasVoted']);
-
-Route::apiResource('admin', AdminController::class);
-
-Route::apiResource('position', PositionController::class);
-
-Route::apiResource('candidate', CandidateController::class);
-Route::patch('/candidates/{id}/increment-votes', [CandidateController::class, 'incrementVotes']);
-
-
-Route::apiResource('voted', VoteController::class);
